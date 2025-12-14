72ffc9c856425a4e0846a7d35acc0553d07aa3f7
royjones32 <hakancatal2018@gmail.com>
2025-12-14 15:14:13 +0300

    Use MONGODB_URI from env (Atlas) + dotenv

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,18 +1,22 @@
-var mongoose=require("mongoose");
-var dbURI="mongodb://localhost/mekanbul";
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const dbURI = process.env.MONGODB_URI;
+
 mongoose.connect(dbURI);
-mongoose.connection.on("connected",function(){
-    console.log("Mongoose "+dbURI+" adresindeki veritabanına bağlandı.");
+
+mongoose.connection.on('connected', function () {
+  console.log("Mongoose " + dbURI + " adresindeki veritabanına bağlandı.");
 });
-mongoose.connection.on("error",function(){
-    console.log("Mongoose bağlantı hatası.");
+
+mongoose.connection.on('error', function (err) {
+  console.log("Mongoose bağlantı hatası.", err);
 });
-mongoose.connection.on("disconnected",function(){
-    console.log("Mongoose bağlantısı kesildi.");
+
+mongoose.connection.on('disconnected', function () {
+  console.log("Mongoose bağlantısı kesildi.");
 });
-process.on("SIGINT",function(){
-    mongoose.connection.close();
-    console.log("Mongoose uygulama sonlandırma nedeniyle bağlantıyı kapattı.");
-    process.exit(0);
-});
-require("./venue");
+
+// Modeller
+require('./venue');
+
